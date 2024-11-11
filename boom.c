45a0e3410ca44edd779baa7169b8686b737418b0
BrunoLTads <bruno4128@hotmail.com>
2024-11-11 17:28:19 -0300

    feat: atualiza campo quando não encontrar bombas, finaliza 'jogo' quando encontra bomba

---
+++ boom.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  printf("   ____   ____   ____  __  __ _ \n  |  _ \\ / __ \\ / __ \\|  \\/  | | \n  | |_) | |  | | |  | | \\  / | | \n  |  _ <| |  | | |  | | |\\/| | | \n  | |_) | |__| | |__| | |  | |_| \n  |____/ \\____/ \\____/|_|  |_(_) \n ");
+
+  return 0;
+}
+
+/* Site utilizado para conseguir o ASCII https://patorjk.com/software/taag/#p=display&f=Big&t=BOOM!
+Copiei linha por linha e adicionei um "\" extra em cada caso onde ele aparecia para que o compilador
+entendesse que não era pulo de linha e sim obrigatório para decoração.
+*/
--- campo.c
+++ campo.c
@@ -1,18 +1,80 @@
 #include <stdio.h>
 
 int main() {
-    int matriz[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
+	int campo_revelado[5][5] = {
+		{0, -1, -1, 0, 0},
+		{-1, -1, 0, -1, -1},
+		{-1, 0, 0, -1, 0},
+		{0, 0, -1, 0, 0},
+		{-1, 0, -1, -1, 0}
+	};
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%d ", matriz[i][j]);
-        }
-        printf("\n");
-    }
+	int campo[5][5] = {
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0}
+	};
 
-    return 0;
+
+	int linha, coluna;
+	int vidas = 1;
+
+	while (vidas >= 1) {
+		printf("Escolha uma linha e coluna.\n");
+		scanf("%d%d", &linha, &coluna);
+
+
+		if (linha >= 0 && linha < 5 && coluna >= 0 && coluna < 5) {
+			campo[linha][coluna] = campo_revelado[linha][coluna];
+			if (campo_revelado[linha][coluna] == 0){
+				int bombas_redor = 0;
+
+				// aqui é pra percorrer as posições num alcance de 3x3 tomando a posição que o usuário botou sendo o centro ( 0 em i e j ) 
+				for (int i = -1; i <= 1; i++) {
+					for (int j = -1; j <= 1; j++) {
+
+						if (i == 0 && j == 0) continue;
+
+						int linha_vizinha = linha + i;
+						int coluna_vizinha = coluna + j;
+
+						// Aqui conta bomba
+						if (linha_vizinha >= 0 && linha_vizinha < 5 && coluna_vizinha >= 0 && coluna_vizinha < 5) {
+							if (campo_revelado[linha_vizinha][coluna_vizinha] == -1) {
+								bombas_redor++;
+							}
+						}
+					}
+				}
+				
+				// Nova matriz sendo que substituindo a posição escolhida pelo número de bombas adjascentes que detectou
+				campo[linha][coluna] = bombas_redor;
+			}
+			else {if (campo_revelado[linha][coluna] == -1) {
+				campo[linha][coluna] = campo_revelado[linha][coluna];
+				vidas = -1;
+				printf("   ____   ____   ____  __  __ _ \n  |  _ \\ / __ \\ / __ \\|  \\/  | | \n  | |_) | |  | | |  | | \\  / | | \n  |  _ <| |  | | |  | | |\\/| | | \n  | |_) | |__| | |__| | |  | |_| \n  |____/ \\____/ \\____/|_|  |_(_) \n ");
+
+				printf("Deseja jogar novamente? \n");
+				
+			}
+			
+			};
+			// Mostrando o campo atualizado (número de bombas e bomba se foi selecionada)
+				for (int i = 0; i < 5; i++) {
+					for (int j = 0; j < 5; j++) {
+						printf("%d ", campo[i][j]);
+					}
+					printf("\n");
+				}
+			
+		}
+		else {
+			printf("Digite uma posição no alcance.");
+		};
+	};
+
+	return 0;
 }
