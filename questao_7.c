9c222df80335e342780eb45760d279029c2826b1
Bruno Lins Dos Santos Viana <20221014040010@ifrn.local>
2025-04-09 17:59:48 -0300

    atualiza exerc_intro

---
+++ questao_7.c
@@ -0,0 +1,14 @@
+/*
+
+Considere um array A de n números naturais Ai e s um valor natural. Escreva um algoritmo que verfique se existe
+um par de elementos em A cuja soma seja s, ou seja,
+
+∃i, j|0 ≤ i < j < |A| =⇒ Ai + Aj = s
+
+onde |A| é a cardinalidade do array A.
+O algoritmo deve retornar um valor lógico (verdadeiro ou falso) que determina se tal soma existe.
+
+*/
+
+#include <stdio.h>
+
